ddbb103f49d8945b0f3dac4c256f23476dc6a03f
sukui <37778349+sukui24@users.noreply.github.com>
2024-08-07 23:12:46 +0300

    #19 implement mocha tests (#20)
    * Add mocha unit test for AuthService, update other files
    
    * Update env examples
    
    * Revert wrong changes for Environment.ts
    
    * Fix issue that npm run test wasn't using mocha config file
    
    * Database changes, delete TestUtils and move user-register test
    
    * Add more tests and do some improvement changes
    
    * Minor changes
    
    * Fix tests, but push bad test for test purposes
    
    * Fix failing test
    
    * Change examples
    
    * Minor changes
    
    ---------
    
    Co-authored-by: asion <asion.desu@gmail.com>
    Co-authored-by: Asion <46422813+Asion001@users.noreply.github.com>

--- server/src/database/Database.ts
+++ server/src/database/Database.ts
@@ -2,17 +2,19 @@ import { DataSource, EntityTarget, ObjectLiteral } from "typeorm";
 import { AppDataSource } from "./DataSource";
 import { Logger } from "../utils/Logger";
 
-
 export class Database {
-  public dataSource: DataSource;
+  private dataSource: DataSource;
+  private _connected: boolean;
 
-  constructor() {
+  constructor(dataSource: DataSource) {
+    this._connected = false;
     Logger.warn("Connecting to DB...");
 
-    this.dataSource = AppDataSource;
+    this.dataSource = dataSource;
     this.dataSource
       .initialize()
       .then(() => {
+        this._connected = true;
         Logger.info("Connection to DB established");
       })
       .catch((err) => {
@@ -22,8 +24,25 @@ export class Database {
   }
 
   public getRepository(target: EntityTarget<ObjectLiteral>) {
+    if (!this._connected) {
+      throw new Error("DB is not connected");
+    }
     return this.dataSource.getRepository(target);
+  }
+
+  public connect() {
+    this._connected = true;
+    return true;
+  }
+
+  public disconnect() {
+    this._connected = false;
+    return true;
+  }
+
+  public get connected() {
+    return this._connected;
   }
 }
 
-export const db = new Database();
+export const db = new Database(AppDataSource);
