66c58f0b7073f739e4fc62500b26463674626fff
asion <asion.desu@gmail.com>
2024-08-12 23:05:04 +0300

    Add env. Refactor

---
+++ client/lib/src/application/app_init.dart
@@ -0,0 +1,33 @@
+import 'dart:io';
+
+import 'package:flutter/widgets.dart';
+import 'package:flutter_web_plugins/url_strategy.dart';
+import 'package:package_info_plus/package_info_plus.dart';
+
+import 'env.dart';
+
+abstract class AppInit {
+  static Future<void> init() async {
+    WidgetsFlutterBinding.ensureInitialized();
+    AppInit.packageInfo = await PackageInfo.fromPlatform();
+    setUrlStrategy(PathUrlStrategy());
+    print(await getInitInfo());
+  }
+
+  static Future<String> getInitInfo() async {
+    final parameters = {
+      'Version': packageInfo.version,
+      'Build number': packageInfo.buildNumber,
+      'Api Domain': Env.apiUrl.toString(),
+      'Locale': Platform.localeName,
+      'Dart Version': Platform.version,
+    };
+
+    String result =
+        parameters.entries.map((e) => '${e.key}: ${e.value}').join('\n');
+    result = '\n$result';
+    return result;
+  }
+
+  static late PackageInfo packageInfo;
+}
