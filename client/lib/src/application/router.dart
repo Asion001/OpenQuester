3947b5385f45129fcc3138faa1937cd63724d818
asion <asion.desu@gmail.com>
2024-09-02 23:15:09 +0300

    Fix routes

--- client/lib/src/application/router.dart
+++ client/lib/src/application/router.dart
@@ -7,12 +7,11 @@ class AppRouter extends RootStackRouter {
   @override
   List<AutoRoute> get routes => [
         AutoRoute(page: HomeRoute.page, initial: true, children: homeTabs),
-        AutoRoute(page: TestScreenRoute.page, path: '/test'),
       ];
 
   List<AutoRoute> get homeTabs => [
-        AutoRoute(page: ClickerRoute.page, path: '/clicker'),
-        AutoRoute(page: ProfileRoute.page, path: '/profile'),
-        AutoRoute(page: TestScreenRoute.page, path: '/test'),
+        AutoRoute(page: ClickerRoute.page, path: 'clicker'),
+        AutoRoute(page: ProfileRoute.page, path: 'profile'),
+        AutoRoute(page: TestScreenRoute.page, path: 'test'),
       ];
 }
