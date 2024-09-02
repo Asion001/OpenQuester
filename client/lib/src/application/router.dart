28f5d2d8bb235e3e64d339b5cf8dfa626d74a518
asion <asion.desu@gmail.com>
2024-09-02 22:48:38 +0300

    Change paths

--- client/lib/src/application/router.dart
+++ client/lib/src/application/router.dart
@@ -6,17 +6,13 @@ import 'router.gr.dart';
 class AppRouter extends RootStackRouter {
   @override
   List<AutoRoute> get routes => [
-        AutoRoute(
-          page: HomeRoute.page,
-          initial: true,
-          children: homeTabs,
-        ),
+        AutoRoute(page: HomeRoute.page, initial: true, children: homeTabs),
         AutoRoute(page: TestScreenRoute.page, path: '/test'),
       ];
 
   List<AutoRoute> get homeTabs => [
-        AutoRoute(page: ClickerRoute.page),
-        AutoRoute(page: ProfileRoute.page),
-        AutoRoute(page: TestScreenRoute.page),
+        AutoRoute(page: ClickerRoute.page, path: '/clicker'),
+        AutoRoute(page: ProfileRoute.page, path: '/profile'),
+        AutoRoute(page: TestScreenRoute.page, path: '/test'),
       ];
 }
