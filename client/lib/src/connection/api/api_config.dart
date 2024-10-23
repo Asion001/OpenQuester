a1a373856eacbc6835b0f651e20d7638c705a3a3
asion <asion.desu@gmail.com>
2024-10-23 21:46:56 +0300

    fix: Format code. Remove fvmrc. Upgrade dart version

--- client/lib/src/connection/api/api_config.dart
+++ client/lib/src/connection/api/api_config.dart
@@ -1,4 +1,4 @@
-// Openapi Generator last run: : 2024-10-23T21:33:03.829277
+// Openapi Generator last run: : 2024-10-23T21:42:07.070027
 import 'package:openapi_generator_annotations/openapi_generator_annotations.dart';
 
 @Openapi(
