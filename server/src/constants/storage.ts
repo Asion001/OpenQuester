fc542cc9333062e8b781ef479dd88a5650398497
sukui <37778349+sukui24@users.noreply.github.com>
2025-02-09 18:44:07 +0200

    feat: Improve file upload security (#131)
    - Use files checksum identify on s3 side to ensure that given file
    corresponds to asked hash
    
    - Use If-None-Match header to make sure that files is not overriden
    (same filename means identical file, so overrite should never happen)

--- server/src/constants/storage.ts
+++ server/src/constants/storage.ts
@@ -1,3 +1,3 @@
-export const GET_FILE_LINK_EXPIRES_IN = 60 * 15; // 15 min
-export const UPLOAD_FILE_LINK_EXPIRES_IN = 30; // 30 sec
-export const UPLOAD_PACKAGE_LINKS_EXPIRES_IN = 60 * 5; // 5 min
+export const GET_FILE_LINK_EXPIRES_IN = 60 * 60 * 12; // 12 hours
+export const UPLOAD_FILE_LINK_EXPIRES_IN = 60 * 60 * 2; // 2 hours
+export const UPLOAD_PACKAGE_LINKS_EXPIRES_IN = 60 * 60 * 2; // 2 hours
