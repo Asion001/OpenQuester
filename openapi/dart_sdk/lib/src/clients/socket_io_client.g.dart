1c6f2288bb9b68780b568fd3b83e47553c64737d
Asion001 <asion.desu@gmail.com>
2025-04-02 23:47:38 +0300

    feat: Update api dart_sdk packages. Add dart_sdk to make commands

--- openapi/dart_sdk/lib/src/clients/socket_io_client.g.dart
+++ openapi/dart_sdk/lib/src/clients/socket_io_client.g.dart
@@ -9,11 +9,7 @@ part of 'socket_io_client.dart';
 // ignore_for_file: unnecessary_brace_in_string_interps,no_leading_underscores_for_local_identifiers,unused_element,unnecessary_string_interpolations
 
 class _SocketIOClient implements SocketIOClient {
-  _SocketIOClient(
-    this._dio, {
-    this.baseUrl,
-    this.errorLogger,
-  });
+  _SocketIOClient(this._dio, {this.baseUrl, this.errorLogger});
 
   final Dio _dio;
 
@@ -27,22 +23,16 @@ class _SocketIOClient implements SocketIOClient {
     final queryParameters = <String, dynamic>{};
     final _headers = <String, dynamic>{};
     const Map<String, dynamic>? _data = null;
-    final _options = _setStreamType<GameEventSubscription>(Options(
-      method: 'POST',
-      headers: _headers,
-      extra: _extra,
-    )
-        .compose(
-          _dio.options,
-          '/subscription/games',
-          queryParameters: queryParameters,
-          data: _data,
-        )
-        .copyWith(
-            baseUrl: _combineBaseUrls(
-          _dio.options.baseUrl,
-          baseUrl,
-        )));
+    final _options = _setStreamType<GameEventSubscription>(
+      Options(method: 'POST', headers: _headers, extra: _extra)
+          .compose(
+            _dio.options,
+            '/subscription/games',
+            queryParameters: queryParameters,
+            data: _data,
+          )
+          .copyWith(baseUrl: _combineBaseUrls(_dio.options.baseUrl, baseUrl)),
+    );
     final _result = await _dio.fetch<Map<String, dynamic>>(_options);
     late GameEventSubscription _value;
     try {
@@ -67,10 +57,7 @@ class _SocketIOClient implements SocketIOClient {
     return requestOptions;
   }
 
-  String _combineBaseUrls(
-    String dioBaseUrl,
-    String? baseUrl,
-  ) {
+  String _combineBaseUrls(String dioBaseUrl, String? baseUrl) {
     if (baseUrl == null || baseUrl.trim().isEmpty) {
       return dioBaseUrl;
     }
