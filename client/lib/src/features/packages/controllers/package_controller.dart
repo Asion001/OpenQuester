d1612b6ff0ddd8a8a5668f02b4e17057f3c3cc9f
Asion001 <asion.desu@gmail.com>
2025-04-04 14:35:52 +0300

    fix: get package caching and remove race

--- client/lib/src/features/packages/controllers/package_controller.dart
+++ client/lib/src/features/packages/controllers/package_controller.dart
@@ -4,11 +4,12 @@ import 'package:openquester/common_imports.dart';
 
 @Singleton(order: 5)
 class PackageController {
-  PackageResponse? _lastPackage;
+  Future<PackageResponse>? _lastPackage;
 
   Future<PackageResponse> getPackage(int id) async {
-    if (_lastPackage?.id == id) return _lastPackage!;
+    if ((await _lastPackage)?.id == id) return _lastPackage!;
     final result = Api.I.api.packages.getV1PackagesId(id: id);
+    _lastPackage = result;
     return result;
   }
 }
