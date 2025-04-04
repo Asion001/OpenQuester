f1dba9750f9e2ce978c8ad6fe3e0ffdc568e10db
Asion001 <asion.desu@gmail.com>
2025-04-04 17:26:57 +0300

    feat: Use package list as cache

--- client/lib/src/features/packages/controllers/package_controller.dart
+++ client/lib/src/features/packages/controllers/package_controller.dart
@@ -1,14 +1,21 @@
 import 'dart:async';
 
+import 'package:infinite_scroll_pagination/infinite_scroll_pagination.dart';
 import 'package:openquester/common_imports.dart';
 
-@Singleton(order: 5)
+@Singleton(order: 6)
 class PackageController {
   Future<PackageResponse>? _lastPackage;
 
   Future<PackageResponse> getPackage(int id) async {
     if ((await _lastPackage)?.id == id) return _lastPackage!;
-    final result = Api.I.api.packages.getV1PackagesId(id: id);
+    final fromPackageList = getIt<PackagesListController>()
+        .pagingController
+        .items
+        ?.firstWhereOrNull((e) => e.id == id);
+    final result = fromPackageList != null
+        ? Future(() => fromPackageList)
+        : Api.I.api.packages.getV1PackagesId(id: id);
     _lastPackage = result;
     return result;
   }
