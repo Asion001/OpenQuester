811c633c2256e165ed1823e7ec33c8347b5cfdbe
Asion001 <asion.desu@gmail.com>
2025-04-24 20:06:25 +0300

    fix: Check for auth state in game

--- client/lib/src/core/controllers/toast_controller.dart
+++ client/lib/src/core/controllers/toast_controller.dart
@@ -3,14 +3,23 @@ import 'package:openquester/common_imports.dart';
 
 @singleton
 class ToastController {
-  Future<void> show(Object message, {BuildContext? context}) async {
+  Future<void> show(
+    Object message, {
+    BuildContext? context,
+    Duration showFor = const Duration(seconds: 5),
+  }) async {
     final messageText = message.toString();
     final snackBar = SnackBar(
       content: Text(messageText),
       behavior: SnackBarBehavior.floating,
+      duration: showFor,
     );
-    ScaffoldMessenger.maybeOf(
+
+    final controller = ScaffoldMessenger.maybeOf(
       context ?? getIt<AppRouter>().navigatorKey.currentContext!,
     )?.showSnackBar(snackBar);
+
+    // Wait to show
+    await controller?.closed;
   }
 }
