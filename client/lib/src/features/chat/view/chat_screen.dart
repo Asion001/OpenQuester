4d5a9e50a8bc0657f002525a4d6696f2d21f71bb
Asion001 <asion.desu@gmail.com>
2025-04-27 15:02:54 +0300

    chore: format

--- client/lib/src/features/chat/view/chat_screen.dart
+++ client/lib/src/features/chat/view/chat_screen.dart
@@ -24,7 +24,6 @@ class ChatScreen extends WatchingWidget {
       builders: Builders(
         composerBuilder: (context) => const Composer(
           attachmentIcon: null,
-          
         ),
       ),
     );
