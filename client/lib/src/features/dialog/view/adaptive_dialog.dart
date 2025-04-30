28b44a9e50401b26f62f105f7481fe4345985337
Asion001 <asion.desu@gmail.com>
2025-04-30 17:44:34 +0300

    fix: Game creation dialog issues #190

--- client/lib/src/features/dialog/view/adaptive_dialog.dart
+++ client/lib/src/features/dialog/view/adaptive_dialog.dart
@@ -4,9 +4,11 @@ import 'package:openquester/openquester.dart';
 class AdaptiveDialog extends StatefulWidget {
   const AdaptiveDialog({
     required this.builder,
+    this.allowBottomSheet = true,
     super.key,
   });
   final Widget Function(BuildContext) builder;
+  final bool allowBottomSheet;
 
   @override
   State<AdaptiveDialog> createState() => _AdaptiveDialogState();
@@ -16,25 +18,38 @@ class _AdaptiveDialogState extends State<AdaptiveDialog>
     with TickerProviderStateMixin {
   @override
   Widget build(BuildContext context) {
-    final wideMode = UiModeUtils.wideModeOn(context);
-    final child = widget.builder(context);
+    final showDialog =
+        !widget.allowBottomSheet || UiModeUtils.wideModeOn(context);
 
-    return Scaffold(
-      backgroundColor: Colors.transparent,
-      body: wideMode
-          ? DialogContainer(child: child)
-          : GestureDetector(onTap: () => Navigator.pop(context)),
-      bottomSheet: wideMode
-          ? const SizedBox() // Fixes child dublicates
-          : BottomSheet(
-              onClosing: () {},
-              showDragHandle: true,
-              animationController: BottomSheet.createAnimationController(this),
-              constraints: const BoxConstraints.tightFor(
-                height: UiModeUtils.maximumDialogWidth,
-              ),
-              builder: (context) => child,
-            ),
+    final child = GestureDetector(
+      child: widget.builder(context),
+      onTap: () {/* do nothing—this swallows the tap */},
+    );
+
+    return Material(
+      color: Colors.transparent,
+      child: GestureDetector(
+        onTap: () => Navigator.pop(context),
+        child: Scaffold(
+          backgroundColor: Colors.transparent,
+          body: showDialog
+              ? DialogContainer(child: child)
+              : GestureDetector(onTap: () => Navigator.pop(context)),
+          bottomSheet: showDialog
+              ? const SizedBox() // Fixes child dublicates
+              : BottomSheet(
+                  elevation: 0,
+                  onClosing: () {},
+                  showDragHandle: true,
+                  animationController:
+                      BottomSheet.createAnimationController(this),
+                  constraints: const BoxConstraints.tightFor(
+                    height: UiModeUtils.maximumDialogWidth,
+                  ),
+                  builder: (_) => child,
+                ),
+        ),
+      ),
     );
   }
 }
