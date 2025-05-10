63180eb6f598b9c899870fc87f05fa6b8d160cf5
Asion001 <asion.desu@gmail.com>
2025-05-11 01:36:10 +0300

    fix: Replace startUpdate func

--- client/lib/src/features/auto_update/view/update_btn.dart
+++ client/lib/src/features/auto_update/view/update_btn.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:openquester/openquester.dart';
+import 'package:updat/theme/dialogs/default.dart';
 import 'package:updat/updat.dart';
 
 class UpdateBtn extends StatelessWidget {
@@ -16,11 +17,30 @@ class UpdateBtn extends StatelessWidget {
       getLatestVersion: getIt<AutoUpdateController>().getLatestVersion,
       getBinaryUrl: (_) async => '',
       appName: LocaleKeys.openquester.tr(),
-      openOnDownload: false,
-      callback: (status) {
-        if (status == UpdatStatus.downloading) {
-          getIt<AutoUpdateController>().openInstallFile();
-        }
+      updateDialogBuilder: ({
+        required appVersion,
+        required changelog,
+        required checkForUpdate,
+        required context,
+        required dismissUpdate,
+        required latestVersion,
+        required launchInstaller,
+        required openDialog,
+        required startUpdate,
+        required status,
+      }) {
+        defaultDialog(
+          context: context,
+          latestVersion: latestVersion,
+          appVersion: appVersion,
+          status: status,
+          changelog: changelog,
+          checkForUpdate: checkForUpdate,
+          openDialog: openDialog,
+          startUpdate: getIt<AutoUpdateController>().openInstallFile,
+          launchInstaller: launchInstaller,
+          dismissUpdate: dismissUpdate,
+        );
       },
     );
   }
