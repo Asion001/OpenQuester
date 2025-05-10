5284e22ce60b15394f59e2ad6fa549a5a07e740e
asion <asion.desu@gmail.com>
2025-05-10 14:32:05 +0300

    feat: Add msix to actions

--- client/lib/src/core/app_init.dart
+++ client/lib/src/core/app_init.dart
@@ -15,14 +15,15 @@ class AppInit {
 
     AppInit.packageInfo = await PackageInfo.fromPlatform();
 
-    // ignore: prefer_const_constructors
-    setUrlStrategy(PathUrlStrategy());
+    setUrlStrategy(const PathUrlStrategy());
+
+    await configureDependencies();
 
     logger.i(await getInitInfo());
   }
 
   static Future<void> buildInit() async {
-    await configureDependencies();
+    // Your init steps
 
     FlutterNativeSplash.remove();
   }
