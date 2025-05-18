558850c5ce73b6a4059f223cae026512e0cdfce6
sukui <37778349+sukui24@users.noreply.github.com>
2025-05-18 20:19:27 +0300

    fix: Players score saving (#219)
    * fix: Players score saving
    
    * fix: cleanup

--- server/src/domain/types/dto/game/room/GameRoomRedisCreateDTO.ts
+++
@@ -1,8 +0,0 @@
-import { Player } from "domain/entities/game/Player";
-
-export interface GameRoomRedisHashDTO {
-  players: Player[];
-  scores: Map<number, number>; // Mapped playerId:score
-  slots: (number | null)[];
-  gameId: string;
-}
