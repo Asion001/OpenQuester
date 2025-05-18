8caa13b1f98dc0a1fd11b5d68a0712edcef369cb
sukui <37778349+sukui24@users.noreply.github.com>
2025-05-18 15:27:08 +0300

    fix: Fix timer object not parsed when get from redis (#216)

--- server/src/application/handlers/TimerExpirationHandler.ts
+++ server/src/application/handlers/TimerExpirationHandler.ts
@@ -12,7 +12,6 @@ import {
   SocketIOGameEvents,
 } from "domain/enums/SocketIOEvents";
 import { ErrorController } from "domain/errors/ErrorController";
-import { GameStateTimerDTO } from "domain/types/dto/game/state/GameStateTimerDTO";
 import { QuestionState } from "domain/types/dto/game/state/QuestionState";
 import { PackageQuestionDTO } from "domain/types/dto/package/PackageQuestionDTO";
 import { RedisExpirationHandler } from "domain/types/redis/RedisExpirationHandler";
@@ -97,16 +96,10 @@ export class TimerExpirationHandler implements RedisExpirationHandler {
       nextState
     );
 
-    const timerRedisValue = await this.gameService.getTimer(
+    const timer = await this.gameService.getTimer(
       game.id,
       QuestionState.SHOWING
     );
-
-    let timer: GameStateTimerDTO | null = null;
-
-    if (timerRedisValue) {
-      timer = JSON.parse(timerRedisValue);
-    }
 
     game.setTimer(timer);
     await this.gameService.updateGame(game);
