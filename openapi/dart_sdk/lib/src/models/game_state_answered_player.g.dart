ac750793763512adf6417b11707c2e619069c884
Asion001 <asion.desu@gmail.com>
2025-05-22 18:10:12 +0300

    feat: Add SocketIOGameAnswerType

--- openapi/dart_sdk/lib/src/models/game_state_answered_player.g.dart
+++ openapi/dart_sdk/lib/src/models/game_state_answered_player.g.dart
@@ -12,6 +12,7 @@ _GameStateAnsweredPlayer _$GameStateAnsweredPlayerFromJson(
   player: (json['player'] as num).toInt(),
   result: (json['result'] as num).toInt(),
   score: (json['score'] as num).toInt(),
+  answerType: SocketIOGameAnswerType.fromJson(json['answerType'] as String),
 );
 
 Map<String, dynamic> _$GameStateAnsweredPlayerToJson(
@@ -20,4 +21,5 @@ Map<String, dynamic> _$GameStateAnsweredPlayerToJson(
   'player': instance.player,
   'result': instance.result,
   'score': instance.score,
+  'answerType': instance.answerType,
 };
