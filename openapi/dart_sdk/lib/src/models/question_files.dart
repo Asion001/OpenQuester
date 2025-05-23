7954e738c771659bd14fe55b7016a896f6908498
asion <asion.desu@gmail.com>
2025-05-23 23:00:02 +0300

    fix: questionText parsing, check for questionItem type also

--- openapi/dart_sdk/lib/src/models/question_files.dart
+++
@@ -1,8 +0,0 @@
-// coverage:ignore-file
-// GENERATED CODE - DO NOT MODIFY BY HAND
-// ignore_for_file: type=lint, unused_import
-
-import 'package_question_file.dart';
-
-/// Media files for the question
-typedef QuestionFiles = List<PackageQuestionFile>;
